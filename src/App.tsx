33246a7bf88b4279541b8ce2b45199f197249545
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2023-04-17 12:02:51 +0530

    final push

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
 
 /**
@@ -22,6 +23,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false,
     };
   }
 
@@ -30,6 +32,9 @@ class App extends Component<{}, IState> {
    */
   renderGraph() {
     return (<Graph data={this.state.data}/>)
+    if(this.state.showGraph){
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
@@ -39,14 +44,25 @@ class App extends Component<{}, IState> {
     DataStreamer.getData((serverResponds: ServerRespond[]) => {
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+      // this.setState({ data: [...this.state.data, ...serverResponds] });
+
+      let x = 0; 
+      const interval = setInterval(()=> {
+       DataStreamer.getData((serverResponds: ServerRespond[]) => {
+          this.setState({data: serverResponds, showGraph: true,});
+        });
+      });
+      x++;
+      if(x > 1000){
+        clearInterval(interval);
+      }
+    },100);
   }
 
   /**
    * Render the App react component
    */
-  render() {
+  render(): JSX.Element {
     return (
       <div className="App">
         <header className="App-header">
